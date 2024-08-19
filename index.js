b1d6d5f66ca2e17814e0b4f4873fee6386496415
Arshqii <marshq23@gmail.com>
2024-08-19 18:46:03 +0700

    added few changes to result & finished playRound and reset function

--- index.js
+++ index.js
@@ -14,28 +14,61 @@ let computerScore = 0;
 function playRound(usersChoice){
 
     const computerChoice = options[Math.floor(Math.random() * 3)];
-    let result = "";
-    console.log(computerChoice);
+    let result = ""; //initialize result placeholder
 
-    if (usersChoice === computerChoice){
-        result = "ITS A TIE"
-    } else if (usersChoice === 'rock' && computerChoice === 'scissor'){
-        result = "YOU WIN!"
-    } else if (usersChoice === 'paper' && computerChoice === 'rock'){
-        result = "YOU WIN!"
-    } else if (usersChoice === 'paper' && computerChoice === 'rock'){
-        result = "YOU WIN!"
-    } else {
-        result = "YOU LOSE!"
-    }
-
-    userChoiceDisplay.textContent = usersChoice;
-    computerChoiceDisplay.textContent = computerChoice;
-    resultDisplay.textContent = result;
     
+    if (turns != 1){
+        if (usersChoice === computerChoice){
+            result = "ITS A TIE";
+            turns--
+        } else if (usersChoice === 'rock' && computerChoice === 'scissor'){
+            result = "YOU WIN!";
+            userScore++;
+            turns--
+        } else if (usersChoice === 'paper' && computerChoice === 'rock'){
+            result = "YOU WIN!";
+            userScore++;
+            turns--
+        } else if (usersChoice === 'paper' && computerChoice === 'rock'){
+            result = "YOU WIN!";
+            userScore++;
+            turns--
+        } else {
+            result = "YOU LOSE!";
+            computerScore++
+            turns--
+        }
+
+        userChoiceDisplay.textContent = usersChoice;
+        computerChoiceDisplay.textContent = computerChoice;
+        computerScoreDisplay.textContent = computerScore;
+        userScoreDisplay.textContent = userScore;
+        numOfTurnsDisplay.textContent = `User has ${turns} turns left`;
+        resultDisplay.textContent = result;
+    } else {
+        turns--
+        if (userScore == computerScore){
+            alert("NOBODY WON, ITS A TIE");
+            reset();
+        } else if (userScore > computerScore){
+            alert("YEYY YOU WON!!\n" + `User: ${userScore}, Computer: ${computerScore}`);
+            reset();
+        } else {
+            alert("YOU LOST, MAYBE NEXT TIME\n" + `User: ${userScore}, Computer: ${computerScore}`);
+            reset();
+        }
+    }
 };
 
 function reset(){
-
+    turns = 5
+    userScore = 0
+    computerScore = 0
+    userChoiceDisplay.textContent = " ";
+    computerChoiceDisplay.textContent = " ";
+    computerScoreDisplay.textContent = 0;
+    userScoreDisplay.textContent = 0;
+    numOfTurnsDisplay.textContent = `User has 5 turns`;
+    resultDisplay.textContent = "";
 };
 
