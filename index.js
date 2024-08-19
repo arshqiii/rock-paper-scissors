32bfd2b3547de421ced014a0ddb127cc7339e8bd
Arshqii <marshq23@gmail.com>
2024-08-19 17:10:35 +0700

    created few functions

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 console.log("Hello World");
 
-const options = ["rock", "paper", "scissor"];
+const options = ['rock', 'paper', 'scissor'];
 const numOfTurnsDisplay = document.getElementById("numOfTurns");
-const userDisplay = document.getElementById("userChoiceDisplay");
-const computerDisplay = document.getElementById("ComputerChoiceDisplay");
+const userChoiceDisplay = document.getElementById("userChoiceDisplay");
+const computerChoiceDisplay = document.getElementById("ComputerChoiceDisplay");
 const userScoreDisplay = document.getElementById("playerScoreDisplay");
 const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 const resultDisplay = document.getElementById("result");
-const turns = 5
+let turns = 5
 let userScore = 0;
 let computerScore = 0;
 
 function playRound(usersChoice){
 
     const computerChoice = options[Math.floor(Math.random() * 3)];
-    // console.log(computerChoice);
+    let result = "";
+    console.log(computerChoice);
 
+    if (usersChoice === computerChoice){
+        result = "ITS A TIE"
+    } else if (usersChoice === 'rock' && computerChoice === 'scissor'){
+        result = "YOU WIN!"
+    } else if (usersChoice === 'paper' && computerChoice === 'rock'){
+        result = "YOU WIN!"
+    } else if (usersChoice === 'paper' && computerChoice === 'rock'){
+        result = "YOU WIN!"
+    } else {
+        result = "YOU LOSE!"
+    }
+
+    userChoiceDisplay.textContent = usersChoice;
+    computerChoiceDisplay.textContent = computerChoice;
+    resultDisplay.textContent = result;
+    
 };
 
 function reset(){
