7857c99443003500e2239fc770e2bfefd9550549
Arshqii <marshq23@gmail.com>
2024-08-19 15:01:19 +0700

    added few changes to html and css, created const in js, & changed background img

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+const options = ["rock", "paper", "scissor"];
+const numOfTurns = document.getElementById("numOfTurns");
+const userScore = document.getElementById("playerScore");
+const AIScore = document.getElementById("computerScore");
+const result = document.getElementById("result");
+const turns = 5
+
+function playRound(userChoice){
+
+}
+
+function reset(){
+
+}
+
