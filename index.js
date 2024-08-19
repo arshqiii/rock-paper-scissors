eee6f5691efcb3b833a3c9e4a523c5daecb0be17
Arshqii <marshq23@gmail.com>
2024-08-19 15:17:18 +0700

    added reset button & renamed few class

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ console.log("Hello World");
 
 const options = ["rock", "paper", "scissor"];
 const numOfTurns = document.getElementById("numOfTurns");
-const userScore = document.getElementById("playerScore");
-const AIScore = document.getElementById("computerScore");
+const userChoice = document.getElementById("userChoiceDisplay");
+const ComputerChoice = document.getElementById("ComputerChoiceDisplay");
+const userScore = document.getElementById("playerScoreDisplay");
+const AIScore = document.getElementById("computerScoreDisplay");
 const result = document.getElementById("result");
 const turns = 5
 
-function playRound(userChoice){
+function playRound(usersChoice){
 
 }
 
